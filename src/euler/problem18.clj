0a6184f9fcd28b9f066e1f1a0da78351fe9cb54a
gphil <gap023@gmail.com>
2010-10-23 17:24:15 -0400

    Worked on problem 18.

--- src/euler/problem18.clj
+++ src/euler/problem18.clj
@@ -2,7 +2,7 @@
 
 ;; Find the maximum total from top to bottom of the triangle below:
 
-(def triangle-rows
+(def triangle-list
    '((75)
      (95 64)
      (17 47 82)
@@ -17,34 +17,18 @@
      (70 11 33 28 77 73 17 78 39 68 17 57)
      (91 71 52 38 17 14 91 43 58 50 27 29 48)
      (63 66 4  68 89 53 67 30 73 16 69 87 40 31)
-     (4  62 98 27 23 9  70 98 73 93 38 53 60 04 23)))
+     (4  62 98 27 23 9  70 98 73 93 38 53 60  4 23)))
 
-;; start small...
+(defn triangle-list-with-coords
+  [triangle-list]
+  (for [[row-idx row] (map-indexed vector triangle-list)]
+     (for [[col-idx val] (map-indexed vector row)]
+       [[(+ 1 row-idx) (+ 1 col-idx)] val])))
 
-;; 1 row
+(def triangle triangle-list-with-coords)
 
-;; it's just the value in the first row
-
-;; 2 rows
-
-;; it's the value in the first row and the max of the second row (both options are available still)
-
-;; 3 rows -- first "real" case where certain options are impossible
-
-;; it's the value in the first row and the max combination of adjacent values in the second and third rows
-
-;; how to rule out possibilities in order not to brute force the max combo of adjacents?
-
-;; maybe work backwards? work in smaller triangle partitions?
-
-;; how to encode the triangle? to uniquely identify each node and determine adjacency
-;; you need to specify the row and the column.
-
-;; it will also be useful to store the weight and pointers to any adjacent nodes 
-
-(defn node
-  [row col wt nodes]
-  {:row row :col col :wt wt :nodes nodes})
+(defn get-adjacent-nodes
+  [node])
 
 ;;;; the "start" node must have a :row that is 1 less then the "end" node.
 (defn row-adjacent?
@@ -63,42 +47,8 @@
     (row-adjacent? (start :row) (end :row)) 
     (col-adjacent? (start :col) (end :col))))
 
-;; need to go from this
 
-(def mini-triangle
-   '((75)
-     (95 64)
-     (17 47 82)
-     (18 35 87 10)))
 
-;; to this
-
-(def mini-triangle-res
-   '((1 1 75)
-     (2 1 95) (2 2 64)
-     (3 1 17) (3 2 47) (3 3 82)))
-
-(defn index-triangle-rows
-  [triangle]
-  (zipmap (iterate inc 1) triangle))
-
-(index-triangle-rows mini-triangle)
-
-(defn index-triangle-cols
-  [triangle]
-  (map #(zipmap (iterate inc 1) %) triangle))
-
-(index-triangle-rows (index-triangle-cols mini-triangle))
-
-(defn index-items
-  [coll]
-  (interleave (iterate inc 1) coll))
-  
-(index-items '(1 (1 2 32 3) 4 5 6))00
-  
-(partition 2 (index-items (index-triangle-cols mini-triangle)))
-
-(map #(interleave (conj () (first %)) (rest %)) '(1 {1 75}))
 
 
 
